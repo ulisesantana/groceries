5a543e4f71f0203396cc85457196e2b6b7cbabc1
Ulises Santana <ulisesantana@gmail.com>
2023-08-02 22:56:33 +0100

    test(e2e): ✅ add test for updating and deleting items

--- cypress/e2e/item-crud.cy.ts
+++ cypress/e2e/item-crud.cy.ts
@@ -1,28 +1,59 @@
 import { messages } from "../../src/messages";
 import { CategoryBuilder, ItemBuilder } from "../../src/tests/builders";
-import { CategoryCypressHelper } from "../helpers/CategoryCypressHelper";
-import { CypressSherpa } from "../helpers/CypressSherpa";
-import base = Mocha.reporters.base;
+import { CypressHelper } from "../helpers/";
 
 describe("Item CRUD should", () => {
   const baseUrl = "http://localhost:3000";
   const category = CategoryBuilder.random();
-  const sherpa = new CypressSherpa(cy, baseUrl);
-  const categoryHelper = new CategoryCypressHelper(cy, baseUrl);
+  const item = ItemBuilder.init().withCategory(category).build();
+  const cypressHelper = new CypressHelper(cy, baseUrl);
 
-  it("add new item", () => {
-    const newItem = ItemBuilder.random();
+  before(async () => {
+    await cypressHelper.clearIndexedDB();
+  });
+
+  it("add a new item", () => {
     // Check the item doesn't exist
-    sherpa.goToAllItemsListView();
-    cy.get("body").should("not.contain.text", newItem.name);
+    cypressHelper.goToAllItemsListView();
+    cy.get("body").should("not.contain.text", item.name);
     // Create a category
-    categoryHelper.createCategory(category);
+    cypressHelper.createCategory(category);
     // Create an item for that category
-    sherpa.goToCreateItemView();
-    sherpa.getByLabel(messages.itemForm.nameInput).type(newItem.name);
-    sherpa.getByLabel(messages.itemForm.submitButton.create).click();
+    cypressHelper.goToCreateItemView();
+    cypressHelper.getByLabel(messages.itemForm.nameInput).type(item.name);
+    cypressHelper.getByLabel(messages.itemForm.submitButton.create).click();
     // Check the item is created
-    sherpa.goToAllItemsListView();
-    cy.get("body").should("contain.text", newItem.name);
+    cypressHelper.goToAllItemsListView();
+    cy.get("body").should("contain.text", item.name);
+  });
+
+  it("update the item", () => {
+    const oldName = item.name;
+    const newName = "Irrelevant item name";
+    // Select Item
+    cypressHelper.goToAllItemsListView();
+    cy.contains(oldName).click();
+    // Update item
+    cypressHelper.getByLabel(messages.itemForm.nameInput).clear().type(newName);
+    cypressHelper.getByLabel(messages.itemForm.submitButton.update).click();
+    // Check the item is updated
+    cypressHelper.goToAllItemsListView();
+    cy.get("body").should("contain.text", newName);
+    cy.get("body").should("not.contain.text", oldName);
+  });
+
+  it("delete the item", () => {
+    const item = ItemBuilder.init().withCategory(category).build();
+    //Check item does not exist
+    cypressHelper.goToAllItemsListView();
+    cy.get("body").should("not.contain.text", item.name);
+    // Create item
+    cypressHelper.createItem(item);
+    // Delete item
+    cy.contains(item.name).click();
+    cy.contains(messages.removeItemButton.cta).click();
+    cy.contains(messages.removeItemButton.confirm).click();
+    // Check item does not exist anymore
+    cy.get("body").should("not.contain.text", item.name);
   });
 });
--- cypress/helpers/CypressHelper.ts
+++ cypress/helpers/CypressHelper.ts
@@ -1,11 +1,81 @@
-import { CypressSherpa } from "./CypressSherpa";
+import { Category, Item } from "../../src/domain";
+import { routes } from "../../src/infrastructure/ui/routes";
+import { messages } from "../../src/messages";
 
 export class CypressHelper {
-  protected readonly sherpa: CypressSherpa;
   constructor(
-    protected readonly cy: Cypress.cy & CyEventEmitter,
-    baseUrl: string
-  ) {
-    this.sherpa = new CypressSherpa(cy, baseUrl);
+    private readonly cy: Cypress.cy & CyEventEmitter,
+    private readonly baseUrl: string
+  ) {}
+
+  async clearIndexedDB() {
+    const databases = await window.indexedDB.databases();
+
+    return Promise.all(
+      databases.map(
+        (db) =>
+          new Promise((resolve, reject) => {
+            const request = window.indexedDB.deleteDatabase(db.name!);
+
+            request.addEventListener("success", resolve);
+            // Note: we need to also listen to the "blocked" event
+            // (and resolve the promise) due to https://stackoverflow.com/a/35141818
+            request.addEventListener("blocked", resolve);
+            request.addEventListener("error", reject);
+          })
+      )
+    );
+  }
+
+  createCategory(category: Category) {
+    const initialUrl = this.cy.url();
+    this.goToCreateCategoryView();
+    this.getByLabel(messages.categoryForm.nameInput).type(category.name);
+    this.getByLabel(messages.categoryForm.iconInput).type(category.icon);
+    this.getByLabel(messages.categoryForm.colorInput).type(category.color);
+    this.getByLabel(messages.categoryForm.submitButton.create).click();
+    initialUrl.then((url) => this.visit(url));
+  }
+
+  createItem(item: Item) {
+    const initialUrl = this.cy.url();
+    this.goToCreateItemView();
+    this.getByLabel(messages.itemForm.nameInput).clear().type(item.name);
+    this.getByLabel(messages.itemForm.quantityInput)
+      .clear()
+      .type(String(item.quantity));
+    this.getByLabel(messages.itemForm.categoryInput).select(
+      item.category.title
+    );
+    this.setCheckbox(messages.itemForm.isRequiredInput, item.isRequired);
+    this.setCheckbox(messages.itemForm.isRequiredInput, item.isMandatory);
+    this.getByLabel(messages.itemForm.submitButton.create).click();
+    initialUrl.then((url) => this.visit(url));
+  }
+
+  getByLabel(label: string) {
+    return this.cy.get(`[aria-label="${label}"]`);
+  }
+
+  goToAllItemsListView() {
+    this.cy.visit(this.baseUrl);
+    this.getByLabel(messages.menu.allItemsListCTA).click();
+  }
+
+  goToCreateCategoryView() {
+    this.cy.visit(this.baseUrl + routes.categories.create);
+  }
+
+  goToCreateItemView() {
+    this.cy.visit(this.baseUrl);
+    this.getByLabel(messages.menu.createItem).click();
+  }
+
+  setCheckbox(label: string, checked: boolean) {
+    this.getByLabel(label)[checked ? "check" : "uncheck"]();
+  }
+
+  visit(url: string) {
+    this.cy.visit(url);
   }
 }
